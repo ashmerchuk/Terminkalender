916c14a7983d0f5a9b703de7cac2f66f4cd8e4b3
Andrii <and.shmerchuk@gmail.com>
2021-05-04 20:28:25 +0200

    first

---
+++ Terminkalender/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Terminkalender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.addEvent = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.chDatum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chZeit = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chEventbeschreibung = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.setCurrenltyTime = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // addEvent
+            // 
+            this.addEvent.Location = new System.Drawing.Point(477, 414);
+            this.addEvent.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.addEvent.Name = "addEvent";
+            this.addEvent.Size = new System.Drawing.Size(252, 33);
+            this.addEvent.TabIndex = 0;
+            this.addEvent.Text = "Add event";
+            this.addEvent.UseVisualStyleBackColor = true;
+            this.addEvent.Click += new System.EventHandler(this.addEvent_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(477, 111);
+            this.textBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(252, 295);
+            this.textBox.TabIndex = 1;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(477, 46);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(252, 24);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(248, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Terminkalender";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chDatum,
+            this.chZeit,
+            this.chEventbeschreibung});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 46);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(459, 401);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseUp);
+            // 
+            // chDatum
+            // 
+            this.chDatum.Text = "Datum";
+            this.chDatum.Width = 80;
+            // 
+            // chZeit
+            // 
+            this.chZeit.Text = "Zeit";
+            this.chZeit.Width = 100;
+            // 
+            // chEventbeschreibung
+            // 
+            this.chEventbeschreibung.Text = "Eventbeschreibung";
+            this.chEventbeschreibung.Width = 275;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(108, 48);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(477, 80);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.ShowUpDown = true;
+            this.dateTimePicker2.Size = new System.Drawing.Size(98, 24);
+            this.dateTimePicker2.TabIndex = 6;
+            // 
+            // setCurrenltyTime
+            // 
+            this.setCurrenltyTime.Location = new System.Drawing.Point(581, 80);
+            this.setCurrenltyTime.Name = "setCurrenltyTime";
+            this.setCurrenltyTime.Size = new System.Drawing.Size(148, 23);
+            this.setCurrenltyTime.TabIndex = 7;
+            this.setCurrenltyTime.Text = "set currently time";
+            this.setCurrenltyTime.UseVisualStyleBackColor = true;
+            this.setCurrenltyTime.Click += new System.EventHandler(this.setCurrenltyTime_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(741, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(741, 479);
+            this.Controls.Add(this.setCurrenltyTime);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.addEvent);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Artifakt Element", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Terminkalender";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addEvent;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader chDatum;
+        private System.Windows.Forms.ColumnHeader chZeit;
+        private System.Windows.Forms.ColumnHeader chEventbeschreibung;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button setCurrenltyTime;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Terminkalender/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace Terminkalender
+{
+    public partial class Form1 : Form
+    {
+
+        private TimeSpan timeLeft;
+        System.Timers.Timer t;
+        public ListViewItem Selected { get; private set; }
+
+        public Form1()
+        {
+            t = new System.Timers.Timer();
+            t.Interval = 1000;
+            t.Elapsed += OnTimeEvent;
+         //   timer.addEvent_Click += Timer_addEvent_Click;
+            InitializeComponent();
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void addEvent_Click(object sender, EventArgs e)
+        {
+            ListViewItem lvi = new ListViewItem(dateTimePicker1.Value.Date.ToString("dd/MM/yyyy"));
+            lvi.SubItems.Add(dateTimePicker2.Value.ToString("HH/mm/ss"));
+            lvi.SubItems.Add(textBox.Text);
+            listView1.Items.Add(lvi);
+            textBox.Text ="";
+            t.Start();
+            DateTime currentTime = DateTime.Now;
+          //  currentTime.ToString("dd/MM/yyyy");
+            DateTime userDateTime = dateTimePicker1.Value;
+            DateTime userJustTime = dateTimePicker2.Value;
+            //    currentTime+
+            t.Interval = 1000;
+
+            timeLeft = new TimeSpan(userJustTime.Second - currentTime.Second);
+            timer1.Start();
+
+            
+            if (currentTime.Second == userJustTime.Second)
+                //&& currentTime.Minute >= userJustTime.Minute &&
+                //currentTime.Second >= userJustTime.Second && currentTime.Day >= userDateTime.Day &&
+                //currentTime.Month >= userDateTime.Month && currentTime.Year >= userDateTime.Year)
+                
+            
+            {
+                // timer.Stop();
+                t.Interval = 1000;
+                MessageBox.Show("Ring, ring...");
+            }
+
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("If you have some questions about program, write me: and.shmerchuk@gmail.com");
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            string textMessage = Console.ReadLine();
+        }
+
+        public void setCurrenltyTime_Click(object sender, EventArgs e)
+        {
+           //  DateTime timenNow = DateTime.Now;
+            dateTimePicker1.Value = DateTime.Now;
+            dateTimePicker2.Value = DateTime.Now;
+
+          //  DateTime setTime = dateTimePicker2.Value;
+          //  string zeitString = Convert.ToString(setTime);
+
+
+        }
+
+        private void listView1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(MousePosition, ToolStripDropDownDirection.Right);
+                
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.SelectedItems[0].Remove();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft = timeLeft.Subtract(TimeSpan.FromSeconds(1));
+        }
+    }
+}
